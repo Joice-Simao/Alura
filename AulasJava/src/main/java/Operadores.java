a52a729e4719a78c14ceddec17acdb07e440053e
Joice-Simao <joice.simao@hotmail.com>
2024-06-11 23:19:31 -0300

    Add Operadores

---
+++ AulasJava/src/main/java/Operadores.java
@@ -0,0 +1,74 @@
+public class Operadores {
+
+    // Atribuir valor a uma variável, sinal de "="
+
+    int valor = 10;
+
+    // Operadores de atribuicao combinado, exemplo +=, atribui valor a variável existente.
+    int valor = 10;
+    valor += 15;
+    // Resultado será valor = 25 pq foi atribuido 15
+
+    // Operadores Aritmeticos
+    // Soma "+"
+    int soma = 10 + 10 ;
+
+    // Subtracao "-"
+    int subtracao = 20 - 10;
+
+    // Multiplicacao "*"
+    int multiplicacao = 10 * 3;
+
+    // Divisao "/"
+    int divisao = 10 / 2;
+
+    // Resto "%"
+    int resto = 10 % 3;
+
+    // Operadores Relacionais - comparar valores
+    // Igual a "=="
+    if (10 == 10){
+        //executa bloco de codigo
+    }
+
+    // Diferente de "!="
+    if (1 != 10){
+        //executa bloco de codigo
+    }
+
+    // Maior que ">"
+    if (10 > 1){
+        //executa bloco de codigo
+    }
+
+    // Maior ou igual a ">="
+    if (10 >= 10){
+        //executa bloco de codigo
+    }
+
+    // Menor que "<"
+    if (1 < 10){
+        //executa bloco de codigo
+    }
+
+    // Menor ou igual a "<="
+    if (10 <= 10){
+        //executa bloco de codigo
+    }
+
+    // Operadores Logicos - verificar duas ou mais condicoes
+
+    // AND "&&"
+
+    // OR "ll"
+
+    // NOT "!"
+
+    // Operadores Incremento - aumenta o valor de uma variavel
+
+    // Pre incremento aumenta o valor antes de usar a variavel
+    ++variavel;
+
+    // Pos incremento aumenta o valor depois de usar a variavel
+    variavel++;
+}
